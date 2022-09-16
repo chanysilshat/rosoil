22e9447a3f1445b85c00f5f70185df0c8e2f3435
ilshat <chanys@yandex.ru>
2022-09-17 00:57:19 +0300

    Дизельное топливо начало

---
+++ d_oil.php
@@ -0,0 +1,20 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("История компании");
+?>
+    <main class="main">  
+        <section class="company-history">
+            <div class="company-history-top" style="background: url(<?=SITE_TEMPLATE_PATH?>/img/company-history.png) no-repeat center center;">
+                <h1 class="company-historyh1">История компании</h1>
+            </div>
+            <div class="page-info">
+                <div class="page-info-text">
+                    <span>
+                        Для получения дизельного топлива нефть подвергают переработке с использованием ректификационных колонн, где она нагревается до нужной температуры и делится на фракции. Дизельная фракция — около четвертой части — получается при нагреве до 180-360 °C. Остальное получают в результате крекинг-процессов с катализаторами в виде металла, водорода или температуры. На этапе крекинга получают до 80% объема дизельного топлива. В конце его подвергают щелочной очистке от примесей.
+                    </span>
+                </div>
+            </div>
+        </section>
+
+    </main>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
