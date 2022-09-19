f303239c3acb61666db0a111dfdb6b0249b427be
ilshat <chanys@yandex.ru>
2022-09-19 23:23:14 +0300

    Первые страницы Марата

---
+++ bitumPMB.php
@@ -0,0 +1,170 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle('Экспорт');
+?>
+   <div class="bitumPMB-conteiner">
+        <div class="marki-block">
+            <div class="marki-text">
+                Марки битума ПМБ в наличии
+            </div>
+        </div>
+        <div class="marki-info-block">
+            <div class="marki-info-text">
+                ПМБ 100/130
+                <br>
+                СТО 2.30-2016
+            </div>
+            <div class="marki-info-text">
+                ПМБ 70/100
+                <br>
+                СТО 2.30-2016
+            </div>
+            <div class="marki-info-text">
+                ПМБ 50/70
+                <br>
+                СТО 2.30-2016
+            </div>
+            <div class="marki-info-text">
+                ПМБ 35/50
+                <br>
+                СТО 2.30-2016
+            </div>
+            <div class="marki-info-text">
+                PMB 35/50
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 75/30-55 (ПБВ-90)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                PMB 40/100-48 (ПБВ-60)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 75/130-75 (ПБВ-60)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 65/105-75 (ПБВ-60)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 40/100-75 (ПБВ-60)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 65/105-48 (ПБВ-60)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 65/105-60 (ПБВ-60)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 40/100-65 (ПБВ-60)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                PMB 25/55-65 (ПБВ-40)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 35/55-55 (ПБВ-40)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 45/80-65 (ПБВ-40)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 45/80-55 (ПБВ-40)
+                <br>
+                EN 14023:2010
+            </div>
+            <div class="marki-info-text">
+                РМВ 25/55-60 (ПБВ-40)
+                <br>
+                EN 14023:2010
+            </div>
+        </div>
+        <div class="passport-block">
+            <div class="passport-text">
+                <img src="/img/PdfFile.png">
+                <span>Паспорт качества продукци</span>
+                <img src="/img/DownloadIcon.png">
+            </div>
+        </div>
+   </div>
+   <div class="bitumPMB-conteiner">
+        <div class="PMB-info">
+            Что такое битум ПМБ
+        </div>
+        <div class="PMB-text">
+            Битумы ПMB, PmB производят путем объединения обычного битума и полимера. Обеспечивают более высокий уровень качества и улучшенные технологические качества битума.
+            <br><br>
+            Модифицированные битумные материалы повышают качество дорожного покрытия за счет лучших прочностных характеристик, а также повысить срок службы дороги за счет увеличения долговечности.
+        </div>
+   </div>
+   <div class="bitumPMB-conteiner">
+        <div class="polimer-PMB">
+            <div class="polimer-PMB-text">
+                Преимущества полимер-модифицированных битумов ПМБ
+            </div>
+        </div>
+    <div class="polimer-conteiner">
+        <div class="polimer-img">
+            <img src="/img/polimer.png">
+        </div>
+        <div class="polimer-info">
+            <div class="polimer-info-text">
+                <div class="polimer-info-text-numbers">
+                    01
+                </div>
+                <div class="polimer-info-text-a">
+                    Повышается устойчивость к деформации, образованию трещин и колеи.
+                </div>
+            </div>
+            <div class="polimer-info-text">
+                <div class="polimer-info-text-numbers">
+                    02
+                </div>
+                <div class="polimer-info-text-a">
+                    Увеличивается пластичность и теплостойкость в широком диапазоне температур
+                </div>
+            </div>
+            <div class="polimer-info-text">
+                <div class="polimer-info-text-numbers">
+                    03
+                </div>
+                <div class="polimer-info-text-a">
+                    На 40% увеличивается срок службы дорожного полотна
+                </div>
+            </div>
+            <div class="polimer-info-text">
+                <div class="polimer-info-text-numbers">
+                    04
+                </div>
+                <div class="polimer-info-text-a">
+                    На 33% сокращаются затраты на содержание автомобильной дороги
+                </div>
+            </div>
+        </div>
+    </div>
+   </div>
+   <div class="bitumPMB-conteiner">
+
+   </div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
---
+++ coldasphalt.php
@@ -0,0 +1,81 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle('Экспорт');
+?>
+   <div class="coldasphalt-conteiner">
+        <div class="asphalt-info">
+            Области применения
+        </div>
+        <div class="asphalt-text-conteiner">
+            <div class="asphalt-text">
+                <b>Ямочный ремонт</b><br>
+                Оперативно производить ямочный ремонт дорожного покрытия на дорогах местного значения и федеральных трассах
+            </div>
+            <div class="asphalt-text">
+                <b>Восстановление дорожного покрытия вокруг колодцев</b><br>
+                Восстановление дорожного покрытия вокруг колодцев, после штробления и аварийных работ
+            </div>
+            <div class="asphalt-text">
+                <b>Благоустройство дворов</b><br>
+                Благоустройство асфальтового покрытия дворовых и городских территорий и улиц города, круглый год
+            </div>
+            <div class="asphalt-text">
+                <b>Гидрофобный слой</b><br>
+                Удобен в устройстве гидрофобного слоя нефтеналивных терминалов
+            </div>
+        </div>
+   </div>
+   <div class="coldasphalt-conteiner">
+        <div class="asphalt-block">
+            <div class="asphalt-text-block">
+                Преимущества холодного асфальта
+            </div>
+        </div>
+    <div class="coldasphalt-block">
+        <div class="coldasphalt-img">
+            <img src="/img/asphalt.png">
+        </div>
+        <div class="coldasphalt-info">
+            <div class="coldasphalt-info-text">
+                <div class="coldasphalt-info-text-numbers">
+                    01
+                </div>
+                <div class="coldasphalt-info-text-a">
+                    Устойчивость к колейности
+                    На основе органического вяжущего используется дорожный битум БНД 90/130 ГОСТ 22245-90
+                </div>
+            </div>
+            <div class="coldasphalt-info-text">
+                <div class="coldasphalt-info-text-numbers">
+                    02
+                </div>
+                <div class="coldasphalt-info-text-a">
+                    Укладка от -30OС до +45OС
+                    Возможность укладки в температурном диапазоне от -30oС до +45oС.
+                </div>
+            </div>
+            <div class="coldasphalt-info-text">
+                <div class="coldasphalt-info-text-numbers">
+                    03
+                </div>
+                <div class="coldasphalt-info-text-a">
+                    Не требует дополнительно проливки краев
+                    Дополнительная пленка битума препятствует надежному сцеплению старого асфальта с холодной смесью
+                </div>
+            </div>
+            <div class="coldasphalt-info-text">
+                <div class="coldasphalt-info-text-numbers">
+                    04
+                </div>
+                <div class="coldasphalt-info-text-a">
+                    Срок хранения 5 лет
+                    Двухслойные полиэтиленовые мешки увеличивают срок хранения до 5 лет, что дает возможность хранить на открытых или закрытых площадках
+                </div>
+            </div>
+        </div>
+    </div>
+   </div>
+   <div class="coldasphalt-conteiner">
+
+   </div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
