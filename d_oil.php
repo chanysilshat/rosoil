b28e9830bcf9143c7d94a954e97f8e569754eda9
ilshat <chanys@yandex.ru>
2022-09-27 07:27:46 +0300

    Ошибки Арсения, создание нового функционала

--- d_oil.php
+++ d_oil.php
@@ -1,13 +1,71 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
-$APPLICATION->SetTitle("История компании");
+$APPLICATION->SetTitle("Дизельное топливо");
 ?>
+<div class="banner-page" style="background-image: url(/upload/banner.png)">
+    <div class="container">
+        <div class="banner-title">
+            Дизельное топливо на экспорт
+        </div>
+        <div class="banner-text">
+            Дизельное топливо (ДТ) — это нефтепродукт, состоящий из смеси углеводородов, которые получают методом перегонки и отбора из них определенных фракций.
+        </div>
+    </div>
+</div>
+<?$APPLICATION->IncludeComponent(
+	"bitrix:news.detail",
+	"banner_page",
+	Array(
+		"ACTIVE_DATE_FORMAT" => "d.m.Y",
+		"ADD_ELEMENT_CHAIN" => "N",
+		"ADD_SECTIONS_CHAIN" => "Y",
+		"AJAX_MODE" => "N",
+		"AJAX_OPTION_ADDITIONAL" => "",
+		"AJAX_OPTION_HISTORY" => "N",
+		"AJAX_OPTION_JUMP" => "N",
+		"AJAX_OPTION_STYLE" => "Y",
+		"BROWSER_TITLE" => "-",
+		"CACHE_GROUPS" => "Y",
+		"CACHE_TIME" => "36000000",
+		"CACHE_TYPE" => "A",
+		"CHECK_DATES" => "Y",
+		"DETAIL_URL" => "",
+		"DISPLAY_BOTTOM_PAGER" => "Y",
+		"DISPLAY_DATE" => "Y",
+		"DISPLAY_NAME" => "Y",
+		"DISPLAY_PICTURE" => "Y",
+		"DISPLAY_PREVIEW_TEXT" => "Y",
+		"DISPLAY_TOP_PAGER" => "N",
+		"ELEMENT_CODE" => "",
+		"ELEMENT_ID" => $_REQUEST["ELEMENT_ID"],
+		"FIELD_CODE" => array("", ""),
+		"IBLOCK_ID" => "",
+		"IBLOCK_TYPE" => "content",
+		"IBLOCK_URL" => "",
+		"INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+		"MESSAGE_404" => "",
+		"META_DESCRIPTION" => "-",
+		"META_KEYWORDS" => "-",
+		"PAGER_BASE_LINK_ENABLE" => "N",
+		"PAGER_SHOW_ALL" => "N",
+		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TITLE" => "Страница",
+		"PROPERTY_CODE" => array("", ""),
+		"SET_BROWSER_TITLE" => "Y",
+		"SET_CANONICAL_URL" => "N",
+		"SET_LAST_MODIFIED" => "N",
+		"SET_META_DESCRIPTION" => "Y",
+		"SET_META_KEYWORDS" => "Y",
+		"SET_STATUS_404" => "N",
+		"SET_TITLE" => "Y",
+		"SHOW_404" => "N",
+		"STRICT_SECTION_CHECK" => "N",
+		"USE_PERMISSIONS" => "N",
+		"USE_SHARE" => "N"
+	)
+);?>
     <main class="main">  
         <section class="company-history">
-            <div class="company-history-top" style="background: url(<?=SITE_TEMPLATE_PATH?>/img/company-history.png) no-repeat center center;">
-                <h1 class="company-historyh1">История компании</h1>
-            </div>
-
             <div class="page-info">
                 <div class="page-info-text">
                     <span>
@@ -51,89 +109,91 @@ $APPLICATION->SetTitle("История компании");
                     </div>
                 </div>
             </div>
-        
-        <div class="polimer-PMB">
-            <div class="polimer-PMB-text">
-                Применение дизельного топлива характерно для заправки практически любых видов транспорта: 
-            </div>
-        </div>
-            
-
-        <div class="polimer-conteiner">
-        <div class="polimer-img">
-            <img src="/img/coil.png">
-        </div>
-        <div class="polimer-info">
-            <div class="polimer-info-text">
-                <div class="polimer-info-text-numbers">
-                    01
-                </div>
-                <div class="polimer-info-text-a">
-                    Легковых и грузовых автомобилей
+            <div class="container">
+                <div class="polimer-PMB">
+                    <div class="polimer-PMB-text">
+                        Применение дизельного топлива характерно для заправки практически любых видов транспорта: 
+                    </div>
                 </div>
             </div>
-            <div class="polimer-info-text">
-                <div class="polimer-info-text-numbers">
-                    02
-                </div>
-                <div class="polimer-info-text-a">
-                    Водного транспорта
+                
+            <div class="container">
+                <div class="polimer-conteiner">
+                    <div class="polimer-img">
+                        <img src="/img/coil.png">
+                    </div>
+                    <div class="polimer-info">
+                        <div class="polimer-info-text">
+                            <div class="polimer-info-text-numbers">
+                                01
+                            </div>
+                            <div class="polimer-info-text-a">
+                                Легковых и грузовых автомобилей
+                            </div>
+                        </div>
+                        <div class="polimer-info-text">
+                            <div class="polimer-info-text-numbers">
+                                02
+                            </div>
+                            <div class="polimer-info-text-a">
+                                Водного транспорта
+                            </div>
+                        </div>
+                        <div class="polimer-info-text">
+                            <div class="polimer-info-text-numbers">
+                                03
+                            </div>
+                            <div class="polimer-info-text-a">
+                                Сельскохозяйственной спецтехники
+                            </div>
+                        </div>
+                        <div class="polimer-info-text">
+                            <div class="polimer-info-text-numbers">
+                                04
+                            </div>
+                            <div class="polimer-info-text-a">
+                                Железнодорожных составов
+                            </div>
+                        </div>
+                        <div class="polimer-info-text">
+                            <div class="polimer-info-text-numbers">
+                                05
+                            </div>
+                            <div class="polimer-info-text-a">
+                                Строительной спецтехники
+                            </div>
+                        </div>
+                        <div class="polimer-info-text">
+                            <div class="polimer-info-text-numbers">
+                                06
+                            </div>
+                            <div class="polimer-info-text-a">
+                                Военной техники
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="polimer-info-text">
-                <div class="polimer-info-text-numbers">
-                    03
-                </div>
-                <div class="polimer-info-text-a">
-                    Сельскохозяйственной спецтехники
-                </div>
-            </div>
-            <div class="polimer-info-text">
-                <div class="polimer-info-text-numbers">
-                    04
-                </div>
-                <div class="polimer-info-text-a">
-                    Железнодорожных составов
-                </div>
-            </div>
-            <div class="polimer-info-text">
-                <div class="polimer-info-text-numbers">
-                    05
-                </div>
-                <div class="polimer-info-text-a">
-                    Строительной спецтехники
-                </div>
-            </div>
-            <div class="polimer-info-text">
-                <div class="polimer-info-text-numbers">
-                    06
-                </div>
-                <div class="polimer-info-text-a">
-                    Военной техники
-                </div>
-            </div>
-        </div>
-    </div>
 
     
 
         <div class='contains'>
             <div class="block-huge">
+                <div class="container">
+                    <div class="polimer-PMB">
+                        <div class="polimer-PMB-text">
+                            Классификация дизельного топлива в Европе стандарт (EN 590), EURO 5 (Евро 5).
+                        </div>
+                    </div>
 
-                <div class="polimer-PMB">
-                    <div class="polimer-PMB-text">
-                        Классификация дизельного топлива в Европе стандарт (EN 590), EURO 5 (Евро 5).
+                    <div class="polimer-txt">
+                        <div>
+                            Распространённая среди потребителей классификация Евро-5 первая после советских ГОСТов получила широкое распространение при маркировке российского дизеля. Связано это больше
+                            с тем, что по нашим дорогам ездит больше зарубежных автомобилей, чем отечественных. 
+                            На законодательном уровне евростандарт EN 590-2009 у нас не действует, но описанные в нём пункты лежат в основе российского ГОСТ 32511-2013
+                        </div>
                     </div>
                 </div>
-
-                <div class="polimer-txt">
-                    <div>
-                        Распространённая среди потребителей классификация Евро-5 первая после советских ГОСТов получила широкое распространение при маркировке российского дизеля. Связано это больше
-                        с тем, что по нашим дорогам ездит больше зарубежных автомобилей, чем отечественных. 
-                        На законодательном уровне евростандарт EN 590-2009 у нас не действует, но описанные в нём пункты лежат в основе российского ГОСТ 32511-2013
-                    </div>
-                </div>
-
                 <div class="block-right">
                     <div class="page-info">
 
@@ -151,12 +211,13 @@ $APPLICATION->SetTitle("История компании");
                     </div>
                 </div>
             </div>
-
-            <div class="polimer-txt12">
+            <div class="container">
+                <div class="polimer-txt12">
                     <div>
                         Дизельное топливо соответствуют ГОСТам и СТО/ТУ и обеспечено сертификатами соответствия и паспортами качества заводов-производителей.  Вы можете ознакомиться в разделе Паспорта качеств и сертификаты соответствия
                     </div>
                 </div>
+            </div>
         </div>
 
     <div class="passport-block">
@@ -243,7 +304,7 @@ $APPLICATION->SetTitle("История компании");
 </div>
 
 <div class="implementation">
-    <div class="container">
+
         <div class="implementation-item">
             <div class="title-prewie implementation">
                 Реализация автомобильного бензина на экспорт 
@@ -254,71 +315,15 @@ $APPLICATION->SetTitle("История компании");
                 и далее связавшись с специалистами экспортного отдела светлых нефтепродуктов 
                 по телефону +7&nbsp;800&nbsp;222-92-25.
             </div>
+            <div class="black-line">
+            </div>
         </div>
-    </div>
+       
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <div class="oil-products">
     <div class="container">
-        <div class="title-prewie oil-products">
+        <div class="title-prewie">
             Сегодня к реализации предлагаются следующие виды сырья и нефтепродуктов:
         </div>
         <div class="kinds">
--- local/templates/rosoil/js/main.js
+++ local/templates/rosoil/js/main.js
@@ -6,4 +6,10 @@ document.addEventListener("DOMContentLoaded", function(){
             this.classList.toggle("open");
         })
     })
+
+    let about_prod = document.querySelector(".about-productions .about-productions-head");
+
+    about_prod.addEventListener('click', function(){
+        this.closest('.about-productions').classList.toggle('open');
+    })
 })
